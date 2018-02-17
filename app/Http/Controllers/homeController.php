0f4a235184d0c3232fa19a23e79e6bcb3ec5651c
smilicat <kirils.vol@gmail.com>
2018-02-17 15:44:40 +0200

    testing

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use home;
 
 class homeController extends Controller
 {
-    //
+  public function index()
+  {
+    $data = home::all();
+    $sum = 5+6;
+    //return $sum;
+    return view('welcome',compact('sum'));
+  }
 }
